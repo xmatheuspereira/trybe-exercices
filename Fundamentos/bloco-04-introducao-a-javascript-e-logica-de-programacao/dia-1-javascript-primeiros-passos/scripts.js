c43b415b1c783a9ad7aa6605b790d002b904dccf
Matheus Pereira <matheusdimmy@hotmail.com>
2021-11-05 15:44:50 -0300

    Inicia um novo bloco

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,11 @@
+const myName = "Matheus";
+const birthCity = "Iporá";
+let birthYear = 1995;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
