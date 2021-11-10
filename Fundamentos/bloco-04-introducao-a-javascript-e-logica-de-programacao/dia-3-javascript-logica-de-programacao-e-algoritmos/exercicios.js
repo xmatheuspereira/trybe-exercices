eee2c269e88839fe1be805a54c9b2e45aef44281
Matheus Pereira <matheusdimmy@hotmail.com>
2021-11-09 22:34:58 -0300

    Finaliza segundo exercicio

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -10,3 +10,12 @@ for (let i = 0; i < n; i += 1){
 }
 
 
+for (let i = 0; i < n; i += 1){
+    let x = '';
+
+    for(let j = 0; j <= i; j += 1){
+    x = x + '*'
+    
+    }
+    console.log(x);
+}
