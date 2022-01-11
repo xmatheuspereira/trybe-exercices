c38fab5ac0ca47b8151852b9f64ef62f076054c8
Matheus Pereira <matheusdimmy@hotmail.com>
2022-01-11 20:47:26 -0300

    Adiciona exercicios

--- Fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/callbacks1.js
+++ Fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/callbacks1.js
@@ -1,15 +1,15 @@
 const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
 const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
-const getUser = () => {
+const getUser = (callback) => {
   const userToReturn = {
     firstName: "Ivan",
     lastName: "Ivanovich",
-    nationality: "Russian"
+    nationality: "Russian",
   };
-  
-  return (userToReturn);
+
+  return callback(userToReturn);
 };
 
-console.log(userFullName(getUser())); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
-console.log(userNationality(getUser())); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
+console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
